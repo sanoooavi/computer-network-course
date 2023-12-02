ff3c4808ac520414f6b5375c71c8fc7c3fd42361
sinatb <sinatb.dev@gmail.com>
2023-12-03 00:32:57 +0330

    udp completed

---
+++ Hw1/Q2/Server_udp.py
@@ -0,0 +1,20 @@
+from socket import *
+from collections import Counter
+
+
+def transform_udp(client_data):
+    counted = Counter(client_data)
+    return client_data.upper()[::-1] + " , " + counted.most_common()[0][0].upper()
+
+
+udp_socket = socket(AF_INET, SOCK_DGRAM)
+udp_socket.bind(("", 9001))
+print("UDP server started")
+
+while True:
+    data, addr = udp_socket.recvfrom(1024)
+    print(data.decode())
+    if data.decode() == "exit" or not data:
+        print(f"udp client with address {addr} disconnected")
+    udp_response = transform_udp(data.decode())
+    udp_socket.sendto(udp_response.encode(), addr)
--- Hw1/Q2/udp.py
+++ Hw1/Q2/udp.py
@@ -1,16 +1,16 @@
 from socket import *
 server_name = "127.0.0.1"
-server_port = 12000
+server_port = 9001
 
 client_socket = socket(AF_INET, SOCK_DGRAM)
 
 message = input("input the udp Message")
 while message != "exit":
     client_socket.sendto(message.encode(), (server_name, server_port))
-    modified_message, server_address = client_socket.recvfrom(2048)
+    modified_message, server_address = client_socket.recvfrom(1024)
     print("From server:", modified_message.decode())
     message = input("input the udp Message")
 
-client_socket.sendto(message, (server_name, server_port))
+client_socket.sendto(message.encode(), (server_name, server_port))
 client_socket.close()
 
